01f544f9667d12d0076c00421f6fba7f01d2c63d
Pedro Francis Maia Coelho <pedro.coelho.1444301@sga.pucminas.br>
2024-04-18 23:32:57 -0300

    Desenvolvimento Tela Proprietários

--- codigo-fonte/template_model_page/template_model_page.js
+++ codigo-fonte/template_model_page/template_model_page.js
@@ -1,63 +1,60 @@
-function teste() {
+async function coletar_proprietarios() {
+    let result
+    const response = await fetch("../repositories/proprietarios.json")
+    const proprietarios = await response.json().then(response => result = response.proprietarios)
+
+    return result
+}
+
+async function proprietarios_init() {
+
     let table = document.getElementById('table_owners')
 
-    let proprietarios = [
-        [
-            1,
-            "Pedro",
-            2,
-            true
-        ],
-        [
-            2,
-            "Sócratis",
-            1,
-            true
-        ],
-        [
-            3,
-            "Zander",
-            4,
-            false
-        ],
-    ]
+    let proprietarios = await coletar_proprietarios()
 
-    for (var i = 0; i < proprietarios.length; i++) {
-
-        if(proprietarios[i][3] == false){
-            let id = proprietarios[i][0]
-            let nome = proprietarios[i][1]
-            let imoveis = proprietarios[i][2]
-
+    proprietarios.map((proprietario) => {
+        if(proprietario.status) {
             table.innerHTML += `
-            <div class="div_table_row">
-                <div class="table_icon">
-                    <img class="icon" src="../scr/icones/icon_proprietarios_selecionado.png" alt="">
-                </div>
-                <div class="table_id">
-                    ${id}
-                </div>
-                <div class="table_name">
-                    ${nome}
-                </div>
-                <div class="table_property">
-                    ${imoveis}
-                </div>
-                <div class="table_status">
-                    <div class="status_active">
-                        <p>
-                            Ativo
-                        </p>
+                <div class="div_table_row">
+                    <div class="table_icon">
+                        <img class="icon" src="../scr/icones/icon_proprietarios_selecionado.png" alt="">
+                    </div>
+                    <div class="table_id">
+                        ${proprietario.id}
+                    </div>
+                    <div class="table_name">
+                        ${proprietario.nome}
+                    </div>
+                    <div class="table_property">
+                        ${proprietario.imoveis.length}
+                    </div>
+                    <div class="table_status">
+                        <div class=${proprietario.status ? "status_active" : "status_inactive"}>
+                            <p>
+                                ${proprietario.status ? "Ativo" : "Inativo"}
+                            </p>
+                        </div>
+                    </div>
+                    <div class="table_button">
+                        <button class="open_button">
+                            <img class="open_icon" src="../scr/icones/icon_ver_dados.png" alt="">
+                        </button>
                     </div>
                 </div>
-                <div class="table_button">
-                    <button class="open_button">
-                        <img class="open_icon" src="../scr/icones/icon_ver_dados.png" alt="">
-                    </button>
-                </div>
-            </div>
             `
             table.innerHTML += '<hr class="divisor"></hr>'
         }
-    }
+    })
+}
+
+function abrir_popup_filtrar_proprietario() {
+    let popup = document.getElementById('popup_filtrar')
+
+    popup.showModal()
+}
+
+function fechar_popup_filtrar_proprietario() {
+    let popup = document.getElementById('popup_filtrar')
+
+    popup.close()
 }
