a82f3fabacc415b5e46892c789a7d1a0c21b1ab1
Socratis_Gomes <164081143+SocratisGomes@users.noreply.github.com>
2024-04-22 22:55:19 -0300

    Desenvolvimento tela login e primeiro acesso

--- codigo-fonte/template_login/template_login.js
+++ codigo-fonte/template_login/template_login.js
@@ -1,2 +1,33 @@
+function abrir_popup_esqueceu_senha(){
+    let popup_dialog=document.getElementById("popup_esqueceu_senha")
+
+    popup_dialog.showModal()
+}
+function abrir_popup_esqueceu_senha_dois(){
+    let popup_dialogdois=document.getElementById("enviar_senha")
+    let popup_dialog=document.getElementById("popup_esqueceu_senha")
+    popup_dialog.close()
+    popup_dialogdois.showModal()
+
+}
+
+// function fechar_popup(){
+//     let fechar_popup_dialog=document.getElementById("popup_esqueceu_senha")
+
+//     fechar_dialog.close()
+// }
+
+
+// function abrir_popup_filtrar_proprietario() {
+//     let popup = document.getElementById('popup_filtrar')
+
+//     popup.showModal()
+// }
+
+// function fechar_popup_filtrar_proprietario() {
+//     let popup = document.getElementById('popup_filtrar')
+
+//     popup.close()
+// }
 
 
