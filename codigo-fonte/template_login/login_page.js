0f979221a46e27bbf7177c98e66cc59588735bd7
Socratis_Gomes <164081143+SocratisGomes@users.noreply.github.com>
2024-04-28 13:57:46 -0300

    Desenv. template login

--- codigo-fonte/template_login/login_page.js
+++ codigo-fonte/template_login/login_page.js
@@ -32,38 +32,39 @@ function iniciar_banco_usuarios(){
             id: 1,
             nome: "Master",
             tipo: "Administrador",
-            email: "teste@immocontrol.com.br",
+            email: "usuariomaster@immocontrol.com.br",
             senha: "123",
             status: true,
             primeiro_acesso: false
         },
         {
             id: 2,
-            nome: "Pedro",
+            nome: "Usuario Ativo",
             tipo: "Comum",
-            email: "pedro@immocontrol.com.br",
-            senha: "123",
+            email: "usuarioativo@immocontrol.com.br",
+            senha: "123456789",
             status: true,
             primeiro_acesso: true
         },
         {
             id: 3,
-            nome: "Socratis",
+            nome: "Usuario Inativo",
             tipo: "Comum",
-            email: "socratis@immocontrol.com.br",
+            email: "usuarioinativo@immocontrol.com.br",
             senha: "123",
             status: false,
             primeiro_acesso: true
         }
     ]
 
-    localStorage.setItem("usuarios", JSON.stringify(usuarios))    
+    localStorage.setItem("usuarios", JSON.stringify(usuarios))     
 }
 
 function iniciar_banco() {
 
     iniciar_banco_usuarios()
 }
+
 
 function verica_usuario(){
     let pega_usuario = document.getElementById("email").value
@@ -76,16 +77,19 @@ function verica_usuario(){
         if(pega_usuario==usuarios[i].email && pega_senha==usuarios[i].senha && usuarios[i].status){
             autenticado = true
             primeiro_acesso = usuarios[i].primeiro_acesso
+            localStorage.setItem('usuario_autenticado', JSON.stringify(usuarios[i])) 
         }        
     }
     if(autenticado && primeiro_acesso){
         let popup_primeiro_acesso = document.getElementById("popup_primeiro_acesso")
-            popup_primeiro_acesso.showModal()
-    }else if(autenticado && !primeiro_acesso){
+            popup_primeiro_acesso.showModal() 
+                   
+
+            }else if(autenticado && !primeiro_acesso){
             location.href ="home_page_teste.html"         
-    }else{
-        alert("USUÁRIO OU SENHA INVÁLIDOS")
-    }  
+            }else{
+                alert("USUÁRIO OU SENHA INVÁLIDOS")
+            }  
    
 } 
 
@@ -94,25 +98,31 @@ function alterar_senha(){
     let nova_senha = document.getElementById("senha_nova").value
     let confirmar_senha = document.getElementById("senha_confirmada").value    
     let senha_alterada = null
-    let usuarios_senha_atualizada = JSON.parse(localStorage.getItem("usuarios"))
-    console.log(usuarios_senha_atualizada)
+    let usuario_autenticado = JSON.parse(localStorage.getItem("usuario_autenticado"))
+    console.log(usuario_autenticado)
 
-
-        if(senha_antiga==123 && nova_senha==confirmar_senha){
+          if(senha_antiga==usuario_autenticado.senha && nova_senha==confirmar_senha){
             alert("login")
-            // location.href = "login_page.html"
-            senha_alterada = nova_senha                              
+            // location.href = "../template_model_page/template_model_page.html"
+            usuario_autenticado.senha = nova_senha 
+            usuario_autenticado.primeiro_acesso = false
+            console.log(usuario_autenticado)                            
         }else if(nova_senha!==confirmar_senha && senha_antiga==123){
                 alert("NOVA SENHA DIFERENTE DA SENHA CONFIRMADA") 
         }else if(senha_antiga!==123 && nova_senha==confirmar_senha){
             alert("SENHA ANTIGA INCORRETA")
         }else{alert("SENHAS INCORRETAS")}
-        usuarios_senha_atualizada.push(senha_alterada)
-        localStorage.setItem("teste", JSON.stringify(usuarios_senha_atualizada))    
-        
-        console.log(usuarios_senha_atualizada)
-        // console.log(usuarios_senha_atualizada.map((aux)=>aux.senha))
 
+        let usuarios = JSON.parse(localStorage.getItem("usuarios"))
+        
+        let novos_usuarios = usuarios.map((usuario)=>{
+            if(usuario.id==usuario_autenticado.id){
+                return usuario_autenticado
+            }else{return usuario}
+        })
+        localStorage.setItem('usuarios', JSON.stringify(novos_usuarios))
+        console.log(novos_usuarios)       
+        // usuarios_senha_atualizada.push(senha_alterada)
 
 }
 
@@ -120,8 +130,23 @@ function alterar_senha(){
 function enviar_email(){
     // FAZER A LOGICA PARA ENVIAR SOMENTE PARA O EMAIL CADASTRADO
     let enviar_email = document.getElementById("email_cadastrado").value
+    let usuarios = JSON.parse(localStorage.getItem("usuarios"))
+        
+    let novos_usuarios = usuarios.map((usuario)=>{
+        if(usuario.email==enviar_email){
+            usuario.senha='123'
+            usuario.primeiro_acesso = true
+            return usuario
+        }else{return usuario}
+    })
+    localStorage.setItem('usuarios', JSON.stringify(novos_usuarios))
+    console.log(novos_usuarios)
+
+
+
     let emailEnviado = document.getElementById("email_enviado")
         emailEnviado.innerHTML += enviar_email
+
 }
 
  
