1a827516c4a9ede51a6610736f57a8db57279538
WollingerEduardo <eduardo.wollinger@outlook.com>
2024-05-13 20:55:34 -0400

    Iniciado Desenvolvimento JS Crud Locatarios

--- codigo-fonte/locatarios_page/locatarios_page.js
+++ codigo-fonte/locatarios_page/locatarios_page.js
@@ -18,9 +18,11 @@ function cadastrar_locatario() {
 }
 
 
-// POPUP CADASTRAR DADOS LOCATÁRIO
-function abrir_popup_dados_locatario() {
+// POPUP DADOS LOCATÁRIO
+function abrir_popup_dados_locatario(e) {
     let popup = document.getElementById('popup_dados_locatario')
+    let dados_locatario = JSON.parse(e.dataset.button)
+    console.log(dados_locatario)
 
     popup.showModal()
 }
@@ -101,5 +103,175 @@ function status_inativo() {
         }
     }
 }
+
+
+
+// BANCO DE DADOS DE LOCATÁRIOS
+
+function locatarios_init() {
+    localStorage.clear()
+
+    iniciar_banco()
+
+    locatarios(null, null, null, true)
+}
+
+
+
+
+function iniciar_banco_locatarios() {
+    let locatarios = [
+        {
+            id: 1,
+            nome: "Locatário 01",
+            cpf: "99999999999",
+            estado_civil: "casado",
+            telefone: "31999999999",
+            email: "pedro@exemplo.com",
+            endereco: {
+                tipo_logradouro: "Rua",
+                logradouro: "Afonço Pena",
+                numero: "1",
+                complemento: "A",
+                bairro: "Centro",
+                cidade: "Belo Horizonte",
+                cep: "00000000",
+                uf: "MG"
+            },
+            status: true,
+            status_cobranca: false
+        },
+        {
+            id: 2,
+            nome: "Locatário 02",
+            cpf: "99999999999",
+            estado_civil: "casado",
+            telefone: "31999999999",
+            email: "pedro@exemplo.com",
+            endereco: {
+                tipo_logradouro: "Rua",
+                logradouro: "Afonço Pena",
+                numero: "1",
+                complemento: "A",
+                bairro: "Centro",
+                cidade: "Belo Horizonte",
+                cep: "00000000",
+                uf: "MG"
+            },
+            status: true,
+            status_cobranca: true
+        },
+        {
+            id: 3,
+            nome: "Locatário 03",
+            cpf: "99999999999",
+            estado_civil: "casado",
+            telefone: "31999999999",
+            email: "pedro@exemplo.com",
+            endereco: {
+                tipo_logradouro: "Rua",
+                logradouro: "Afonço Pena",
+                numero: "1",
+                complemento: "A",
+                bairro: "Centro",
+                cidade: "Belo Horizonte",
+                cep: "00000000",
+                uf: "MG"
+            },
+            status: false,
+            status_cobranca: false
+        }
+
+    ]
+    localStorage.setItem("locatarios", JSON.stringify(locatarios))
+}
+
+function iniciar_banco() {
+    iniciar_banco_locatarios()
+}
+
+// Funcão para povoar a tela com o banco de dados
+function locatarios(id, nome, cpf, status) {
+    var node = document.getElementById('table_list')
+    node.innerHTML = ""
+
+    let id_value = id
+    let nome_value = nome
+    let cpf_value = cpf
+
+    if (id_value == "") {
+        id_value = null
+    }
+
+    if (nome_value == "") {
+        nome_value = null
+    }
+
+    if (cpf_value == "") {
+        cpf_value = null
+    }
+
+// PARAMOS AQUI
+
+    let locatarios = JSON.parse(localStorage.getItem("locatarios"))
+    console.log(locatarios)
+
+    let table_locatarios = document.getElementById('table_list')
+
+
+    if(locatarios.length > 0) {
+        locatarios.map((locatario) => {
+            dados_locatario = JSON.stringify(locatario) 
+
+            table_locatarios.innerHTML += `
+            <div class="div_table_row"> 
+                <div class="table_icon">
+                    <img src="../src/icones/icon_locatarios_selecionado.png" alt="" class="icon">
+                </div>
+                <div class="table_id">
+                    ${locatario.id}
+                </div>
+                <div class="table_name">
+                    ${locatario.nome}    
+                </div>
+                <div class="table_telefone">
+                    ${locatario.telefone}
+                </div>
+                <div class="table_email">
+                    ${locatario.email}
+                </div>
+                <div class="table_status">
+                    <div class=${locatario.status ? "status_active" : "status_inactive"}>
+                        <p>
+                            ${locatario.status ? "Ativo" : "Inativo"}
+                        </p>
+                    </div>
+                </div>
+
+                <div class="table_status">
+                    <div class=${locatario.status_cobranca ? "status_active" : "status_inactive"}>
+                        <p>
+                            ${locatario.status_cobranca ? "Adimplente" : "Inadimplente"}
+                        </p>
+                    </div>
+                </div>
+
+
+                <div class="table_button">
+                    <buttom class="open_button" data-button='${dados_locatario}' onclick=" return abrir_popup_dados_locatario(this)"><img src="../src/icones/icon_ver_dados.png" alt="" class="open_icon"></buttom>
+                </div>
+            </div>    
+            <hr class="divisor">        
+            `
+                       
+
+        })
+    }
+
+
+
+
+}
+
 
 
