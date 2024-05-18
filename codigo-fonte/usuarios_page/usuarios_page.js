1b3a219dbc179dc0950c54e7f9f254a830be82c3
Thassia-PUC <tdrumond@sga.pucminas.br>
2024-05-17 21:03:04 -0300

    Primeiro pop up

---
+++ codigo-fonte/usuarios_page/usuarios_page.js
@@ -0,0 +1,66 @@
+// mostrar o pop up filtrar usuários//
+
+function abrir_popup_filtrar_usuarios() {
+    let popup = document.getElementById('popup_filtrar')
+   
+    popup.showModal()
+}
+
+//fechar pop up filtro usuário//
+
+function fechar_popup_filtrar_usuarios() {
+    let popup = document.getElementById('popup_filtrar')
+
+    popup.close()
+}
+
+// validação da caixa de status//
+
+function status_todos() {
+    let status_todos = document.getElementById("status_todos")
+    let status_ativo = document.getElementById("status_ativo")
+    let status_inativo = document.getElementById("status_inativo")
+
+    if (status_todos.checked) {
+        status_ativo.checked = true
+        status_inativo.checked = true
+    } else {
+        status_ativo.checked = false
+        status_inativo.checked = false
+    }
+}
+
+function status_ativo() {
+    let status_todos = document.getElementById("status_todos")
+    let status_ativo = document.getElementById("status_ativo")
+    let status_inativo = document.getElementById("status_inativo")
+
+    if (status_ativo.checked) {
+        if (status_inativo.checked) {
+            status_todos.checked = true
+        }
+    } else {
+        if (status_todos.checked == true) {
+            status_todos.checked = false
+        }
+    }
+}
+
+function status_inativo() {
+    let status_todos = document.getElementById("status_todos")
+    let status_ativo = document.getElementById("status_ativo")
+    let status_inativo = document.getElementById("status_inativo")
+
+    if (status_inativo.checked) {
+        if (status_ativo.checked) {
+            status_todos.checked = true
+        }
+    } else {
+        if (status_todos.checked == true) {
+            status_todos.checked = false
+        }
+    }
+}
+
+
+
