0a1d01cbe1e5a3ae11871ef00b9d4d2a2ff9e7f3
ZanderPereira <zanderpereira@sga.pucminas.br>
2024-05-24 09:03:29 -0300

    atualização

--- codigo-fonte/contratos/contratos.js
+++ codigo-fonte/contratos/contratos.js
@@ -1,88 +1,333 @@
 
+contratos = [
+    {
+        id: 1,
+        locatario: "josé da silva",
+        endereco: "rua são josé, centro",
+        periodo: "12 meses",
+        valor_mensal: "600,00",
+        data_inicio: "23/03/2024",
+        status: "ativo",
+        proprietario: "marcos"
+    },
+    {
+        id: 2,
+        locatario: "fernando abreu",
+        endereco: "rua das nações, observatorio",
+        periodo: "6 meses",
+        valor_mensal: "1600,00",
+        data_inicio: "23/03/2024",
+        status: "inativo",
+        proprietario: "vinicius"
+    },
+    {
+        id: 3,
+        locatario: "Mariilia pereira",
+        endereco: "rua laranjal, paulista",
+        periodo: "24 meses",
+        valor_mensal: "600,00",
+        data_inicio: "23/03/2024",
+        status: "ativo",
+        proprietario: "joana"
+    }
 
-//coletando elemento fade:
-var ElementoFade = document.getElementById("fade")
+]
 
-// coletando os dialogs:
-var dialog1 = document.getElementById("modal1");
-var dialog2 = document.getElementById("modal2");
-var dialog3 = document.getElementById("modal3");
-//---------------------------------------------
+dialog = document.getElementById('modal')
+ElementoFade = document.getElementById('fade')
+var elementoLista = document.getElementById('table_list')
 
-//coletando os iframes dos dialog:
-const iframe1 = document.getElementById("meuIframeFiltrar");
-const iframe2 = document.getElementById("meuIframeadicionar");
-const iframe3 = document.getElementById('meuIframeAlterar')
-//-------------------------------------------------------------
+function fecharModal() {
+    ElementoFade.classList.remove('escuro');
+    dialog.close()
 
-// Ações dos botões:
+}
 
-function abrirModalFiltro(){
+function abrirModalFiltro() {
+
     ElementoFade.classList.add("escuro");
-    dialog3.removeAttribute('open');
-    dialog2.removeAttribute('open');    
-    dialog1.setAttribute('open', 'true');//<-
+    dialog.classList.add("popUpFiltro")
+    dialog.setAttribute('open', 'true');
+
+    dialog.innerHTML = `
+    <h1 class="fil">Filtros</h1>
+    <h5 class="titulos">Dados do Contrato<hr class="hr"></h5>
+    
+
+    <div class="alinhamento" >
+        <label class="titulos" for="id">ID<br>
+            <input type="text" name="id" id="id">
+
+        </label>
+        
+        <label class="titulos" for="proprietario">Proprietário <br>
+            <input type="text" name="proprietario" id="proprietario">
+        </label>
+
+        <label class="titulos" for="locatario">Locatário <br>
+            <input type="text" name="locatario" id="locatario">
+        </label>
+    </div>
+
+        <h4 class="titulos">Status
+            <hr class="hr">
+        </h4>
+
+
+    <div class="checar">
+        
+        <label class="switch">
+            <input type="radio" name="escolha" value="todos" checked ><h7>TODOS</h7>
+        </label>
+        <label class="switch">
+            <input type="radio" name="escolha" value="ativo"><h7>ATIVO</h7>
+        </label>
+        <label class="switch">
+            <input type="radio" name="escolha" value="inativo"><h7>INATIVO</h7>
+        </label>  
+          
+    </div>
+
+
+    <div class="botoes">
+        <button id="voltar_principal" onclick="fecharModal()"><img src="../src/icones/voltar.png" alt=""></button>
+        <button class="flutuar" onclick="filtrarContratos()"><img src="../src/icones/filtrar.png" alt=""></button>
+    </div>
+
+
+    `
+
+
 }
 
 
-function abrirModalAdicao(){
+function abrirModalAdicao() {
+
     ElementoFade.classList.add("escuro");
-    dialog3.removeAttribute('open');
-    dialog1.removeAttribute('open');
-    dialog2.setAttribute('open', 'true');
+    dialog.classList.add('PopUpAdicaoContrato')
+    dialog.setAttribute('open', 'true');
+    dialog.innerHTML = `
+    <h1>Novo Contrato</h1>
+    <h4 class="titulos">LOCATÁRIO
+        <hr>
+    </h4>
+    <label for="locatario">Nome</label>
+    <input class="longo" type="text" name="locatario" id="locatario">
+    <h4 class="titulos">IMÓVEL
+        <hr>
+    </h4>
+
+    <div class="alinhamento">
+        <label class="titulos" for="endereco">ID - ENDEREÇO <br></label>
+        <input class="longo" type="text" name="endereco" id="endereco">
+    </div>
+
+    <div class="alinhamento-a">
+        <label class="titulos" for="endereco">Período <hr> 
+            <input type="text" name="periodo" id="periodo">
+        </label>
+        <label class="titulos" for="valor_mensal">Valor Mensal <hr> 
+            <input type="text" name="valor_mensal" id="valor_mensal">
+        </label>
+    </div>
+
+    
+    <div class="alinhamento-a">
+        
+        <label class="titulos" for="data_inicio">Data de início<hr>
+            <input type="date" name="data_inicio" id="data_inicio">
+        </label>
+        
+    </div>
+
+    <div class="botoes">
+        <button onclick="fecharModal()"><img src="../src/icones/voltar.png" alt=""></button>
+        <button class="flutuar"><img src="../src/icones/salvar.png" alt=""></button>
+    </div>
+    
+    
+    `
+
+
 }
 
-function abrirModalAlteracao(){
-    ElementoFade.classList.add("escuro");
-    dialog1.removeAttribute('open');
-    dialog2.removeAttribute('open');
-    dialog3.setAttribute('open', 'true');
+function abrirModalAlteracao() {
+    
 }
 
-iframe1.onload = function() {
-    // Obtém o conteúdo do iframe
-    var iframeConteudo = iframe1.contentWindow;
 
 
-    // Obtém o botão do iframe
-    var botaoDoIframe = iframeConteudo.document.getElementById("voltar_principal");
-
-    // Adiciona um evento de clique ao botão do iframe
-    botaoDoIframe.addEventListener("click", function() {
-        dialog1.removeAttribute('open');
-        ElementoFade.classList.remove('escuro');
-    });
-};
-
-iframe2.onload = function() {
-    // Obtém o conteúdo do iframe
-    var iframeConteudo = iframe2.contentWindow;
+function filtrarContratos() {
+    //var elementoLista = document.getElementById('table_list');
+    var contratosfiltrados = contratos;
+    var id = document.getElementById('id').value;
+    var proprietario = document.getElementById('proprietario').value.toLowerCase();
+    var locatario = document.getElementById('locatario').value.toLowerCase();
+    var status = document.querySelector('input[type="radio"]:checked').value.toLowerCase();
+    console.log(proprietario)
 
 
-    // Obtém o botão do iframe
-    var botaoDoIframe = iframeConteudo.document.getElementById("voltar_principal");
+    if (id != 0) {
+        contratosfiltrados = contratosfiltrados.filter((contrato) => contrato.id == id);
+    }
 
-    // Adiciona um evento de clique ao botão do iframe
-    botaoDoIframe.addEventListener("click", function() {
-        dialog2.removeAttribute('open');
-        ElementoFade.classList.remove('escuro');
-    });
-};
+    if (proprietario != "") {
+        contratosfiltrados = contratosfiltrados.filter((contrato) => contrato.proprietario.includes(proprietario));
+    }
+    if (locatario != "") {
+        contratosfiltrados = contratosfiltrados.filter((contrato) => contrato.locatario.includes(locatario));
+    }
+    if (status == 'ativo' || status == 'inativo') {
+        contratosfiltrados = contratosfiltrados.filter((contrato) => contrato.status == status);
+    }
+
+    console.log(contratosfiltrados)
+    elementoLista.innerHTML = ""
+    for (let i = 0; i < contratosfiltrados.length; i++) {
+        elementoLista.innerHTML += `
+        <div class="div_table_row">
+  
+      <div class="table_icon1">
+          <img class="icon" src="../src/icones/icon_contratos_selecionado.png" alt="">
+      </div>
+  
+      <div class="table_id" id="id">
+          ${contratosfiltrados[i].id}
+      </div>
+  
+      <div class="table_name" id="name">
+          ${contratosfiltrados[i].locatario}
+      </div>  
+  
+      <div class="table_property" id="tempo">
+          ${contratosfiltrados[i].periodo}
+      </div>
+  
+      <div class="table_status" id="mensalidade">
+          ${contratosfiltrados[i].valor_mensal}
+      </div>
+      <div class="table_status" id="situacao">
+          ${contratosfiltrados[i].status}
+      </div>
+  
+      <div class="table_button">
+          <button class="open_button" onclick="abrirModalAlteracao()">
+              <img class="open_icon" src="../src/icones/icon_ver_dados.png" alt="">
+          </button>
+      </div>
+  
+  </div>`
+
+    }
+
+}
 
 
-iframe3.onload = function() {
-    // Obtém o conteúdo do iframe
-    var iframeConteudo = iframe3.contentWindow;
+function carregarBancoContratos(){
+
+    localStorage.setItem("contratos", JSON.stringify(contratos));
+    elementoLista.innerHTML = ""
+    for (let i = 0; i < contratos.length; i++) {
+        elementoLista.innerHTML += `
+    <div class="div_table_row">
+  
+      <div class="table_icon1">
+          <img class="icon" src="../src/icones/icon_contratos_selecionado.png" alt="">
+      </div>
+  
+      <div class="table_id" id="id">
+          ${contratos[i].id}
+      </div>
+  
+      <div class="table_name" id="name">
+          ${contratos[i].locatario}
+      </div>  
+  
+      <div class="table_property" id="tempo">
+          ${contratos[i].periodo}
+      </div>
+  
+      <div class="table_status" id="mensalidade">
+          ${contratos[i].valor_mensal}
+      </div>
+      <div class="table_status" id="situacao">
+          ${contratos[i].status}
+      </div>
+  
+      <div class="table_button">
+          <button class="open_button" onclick="abrirModalAlteracaoContratos()">
+              <img class="open_icon" src="../src/icones/icon_ver_dados.png" alt="">
+          </button>
+      </div>
+  
+    </div>`
+
+    }
 
 
-    // Obtém o botão do iframe
-    var botaoDoIframe = iframeConteudo.document.getElementById("voltar_principal");
+}
+function abrirModalAlteracaoContratos(){
+    ElementoFade.classList.add("escuro");
+    dialog.setAttribute('open', 'true');
+    dialog.classList.add('PopUpAlterar')
+    dialog.innerHTML=`
+    <h1>Dados do Contrato</h1>
+    <h4 class="titulos">LOCATÁRIO
+        <hr>
+    </h4>
+    <label for="locatario">Nome</label>
+    <input class="longo" type="text" name="locatario" id="locatario">
+    <h4 class="titulos">IMÓVEL
+        <hr>
+    </h4>
 
-    // Adiciona um evento de clique ao botão do iframe
-    botaoDoIframe.addEventListener("click", function() {
-        dialog3.removeAttribute('open');
-        ElementoFade.classList.remove('escuro');
-    });
-};
+    <div class="alinhamento">
+        <label class="titulos" for="endereco">ID - ENDEREÇO <br></label>
+        <input class="longo" type="text" name="endereco" id="endereco">
+    </div>
 
+    <div class="alinhamento-a">
+        <label class="titulos" for="endereco">Período <br>
+            <input type="text" name="periodo" id="periodo">
+        </label>
+        <label class="titulos" for="valor_mensal">Valor Mensal <br>
+            <input type="text" name="valor_mensal" id="valor_mensal">
+        </label>
+    </div>
 
+    
+    <div class="alinhamento-a">
+        
+        <label class="titulos" for="data_inicio">Data de início <hr>
+            <input type="text" name="data_inicio" id="data_inicio">
+        </label>
+        
+        <label class="titulos descer" for="valor_mensal">Status <hr>
+            <div class="centro">
+                <label class="switch">
+                    <input type="checkbox">
+                    <span class="slider round"></span>
+                    <label for="span"></label>
+                </label>
+                <h5>Ativo</h5>
+            </div> 
+        </label>
+        
+        
+    </div>
+
+    <div class="botoes">
+        <button id="voltar_principal"><img src="../src/icones/voltar.png" alt=""></button>
+        <button class="flutuar"><img src="../src/icones/alterar.png" alt=""></button>
+    </div>
+    
+    
+    
+    
+    
+    
+    
+    
+    `
+
+}
